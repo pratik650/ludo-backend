908ec94516ec9789b8d2280cee4289c95d4ca649
Pratik <pratikprajapati650@gmail.com>
2023-12-04 19:29:09 +0530

    feat: backend done

--- src/models/User.js
+++ src/models/User.js
@@ -1,46 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: [true, 'Full name is required'],
-    trim: true
-  },
-  address: {
-    type: String,
-    required: [true, 'Address is required'],
-    trim: true
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-    lowercase: true,
-    trim: true,
-    // match: [/\S+@\S+\.\S+/, 'Please use a valid email address']
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required']
-  },
-  Phonenumber: {
-    type: String,
-    required: [true, 'Phone number is required'],
-    trim: true,
-  },
-  eligible: {
-    type: Boolean,
-    required: [true, 'Eligibility status is required']
-  },
-  available: {
-    type: Boolean,
-    required: [true, 'Availability status is required']
-  }
+    fullName: {
+        type: String,
+        required: [true, 'Full name is required'],
+        trim: true
+    },
+    Phonenumber: {
+        type: String,
+        required: [true, 'Phone number is required'],
+        trim: true,
+    },
+    otp: {
+        type: String, // Add an OTP field to store the generated OTP
+    }
 }, {
-  timestamps: true
+    timestamps: true
 });
 
+const User = mongoose.model('User', userSchema);
 
-const Driver = mongoose.model('User', userSchema);
-
-module.exports = Driver;
+module.exports = User;
